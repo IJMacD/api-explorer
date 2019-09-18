98a8dfe6106d935338d64a44af618fbb00b2d815
Iain MacDonald <iain.ilearner@gmail.com>
2019-09-18 17:01:01 +0800

    Updates

---
+++ precache-manifest.f59ac623a56d4862929ae996b9869ac5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6053cc9039431b8bb0046c11527cb39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f932b3da309dbda9e34",
+    "url": "/static/css/main.30f8e38f.chunk.css"
+  },
+  {
+    "revision": "32fc72acf9a85b4b3935",
+    "url": "/static/js/2.2549a3d3.chunk.js"
+  },
+  {
+    "revision": "6f932b3da309dbda9e34",
+    "url": "/static/js/main.42d696a8.chunk.js"
+  },
+  {
+    "revision": "29aa82b753b84d992d93",
+    "url": "/static/js/runtime~main.8520823e.js"
+  }
+]);
